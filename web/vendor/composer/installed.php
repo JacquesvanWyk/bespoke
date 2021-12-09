ed071a68fe21d25ddaff1b6e6e55d7e4173e257c
Jacques van Wyk <jvw679@gmail.com>
2021-12-09 20:28:51 +0200

    Updates

---
+++ web/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'c063f99c4130f25e6b2503902b3f37564a7e2d4f',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c063f99c4130f25e6b2503902b3f37564a7e2d4f',
+    ),
+    'mwdelaney/sage-acf-gutenberg-blocks' => 
+    array (
+      'pretty_version' => '0.7',
+      'version' => '0.7.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '795527e63b45c8426741a074270355953b28ce37',
+    ),
+  ),
+);
